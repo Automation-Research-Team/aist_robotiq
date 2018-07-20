aef7bc05e5e8fee1b02e18ef9f192e981d02807d
Yuma Hijioka <yuma.hijioka@aist.go.jp>
2018-07-20 17:18:42 +0900

    Various fixed.

--- o2as_vision/scripts/pick_and_place.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import rospy
-from phoxi_camera.srv import *
-from std_srvs.srv import *
-
-if __name__ == '__main__':
-    rospy.init_node('pick_and_place', anonymous=True)
-    while not rospy.is_shutdown():
-        
