a3ce8d8d5ab2081c830fdafe8bcdae3b55911504
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2018-07-20 11:00:45 +0900

    o2as_usb_relay now supports power-controlable hub.

--- o2as_usb_relay/src/o2as_relay_server.cpp
+++ o2as_usb_relay/src/o2as_relay_server.cpp
@@ -1,35 +1,54 @@
+/*!
+ *   \file	o2as_relay_server.cpp
+ */
 #include "ros/ros.h"
-#include "std_srvs/SetBool.h"
+#include "o2as_usb_relay/SetPower.h"
+#include "TU/USB++.h"
 
-// This node is based on the tutorial: http://wiki.ros.org/ROS/Tutorials/WritingServiceClient%28c%2B%2B%29
-
-bool turn_on_relay_1(std_srvs::SetBool::Request  &req,
-         std_srvs::SetBool::Response &res)
+namespace TU
 {
-    if (req.data == true)
-    {
-        ROS_INFO("Turning suction on.");
-        // Insert code to turn on suction here
-    }
-    else 
-    {
-        ROS_INFO("Turning suction off.");
-        // Insert code to turn off suction here
-    }
-
-    res.success = true;
-    res.message = "";
-    return true;
-}
-
-int main(int argc, char **argv)
+struct USBHubAdaptor : public USBHub
 {
-  ros::init(argc, argv, "usb_relay_server");
-  ros::NodeHandle n;
+    USBHubAdaptor()	:USBHub()				{}
 
-  ros::ServiceServer service = n.advertiseService("turn_on_gripper_suction", turn_on_relay_1);
-  ROS_INFO("USB relay server is active.");
-  ros::spin();
+    bool
+    setPower(o2as_usb_relay::SetPower::Request&  req,
+	     o2as_usb_relay::SetPower::Response& res)
+    {
+	ROS_INFO("Turning port %d %s.", req.port, (req.on ? "on" : "off"));
 
-  return 0;
+	try
+	{
+	    USBHub::setPower(req.port, req.on);
+	}
+	catch (const std::exception& err)
+	{
+	    res.success = false;
+	    res.message = err.what();
+	}
+		    
+	res.success = true;
+	res.message = std::string("Port ") + char('0' + req.port)
+		    + " is turned " + (req.on ? "on." : "off.");
+
+	return true;
+    }
+};
+}	// namespace TU
+
+int
+main(int argc, char** argv)
+{
+    ros::init(argc, argv, "o2as_usb_relay_server");
+
+    auto		hub = boost::make_shared<TU::USBHubAdaptor>();
+    ros::NodeHandle	node;
+    auto		service = node.advertiseService(
+					"turn_on_gripper_suction",
+					&TU::USBHubAdaptor::setPower, hub);
+    ROS_INFO("o2as_usb_relay_server is active.");
+
+    ros::spin();
+
+    return 0;
 }
