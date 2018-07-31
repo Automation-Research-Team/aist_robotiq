391420900080486ae8dbc21066f08ae2527230bd
Felix von Drigalski <FvDrigalski@gmail.com>
2018-07-31 17:15:28 +0900

    Finish gripper example, fix URScript driver

--- o2as_moveit_planner/src/action_example.cpp
+++ o2as_moveit_planner/src/action_example.cpp
@@ -3,6 +3,10 @@
 #include <robotiq_msgs/CModelCommandAction.h>
 #include <ros/ros.h>
 
+// This example shows how to open and close the gripper via an action
+// Tested with a real UR and a gripper connected to it. Consult the launch file.
+// Based on http://wiki.ros.org/actionlib_tutorials/Tutorials/SimpleActionClient
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "test_gripper");
@@ -16,9 +20,10 @@ int main(int argc, char **argv)
     b_bot_gripper_client.waitForServer(); // will wait for infinite time
 
     ROS_INFO("Action server started, sending goal.");
+
     // Send a goal to the action
     robotiq_msgs::CModelCommandGoal goal;
-    goal.position = 0.0;    // 0 to close, 0.85 to open the gripper
+    goal.position = 0.0;    // Opening width. 0 to close, 0.085 to open the gripper.
     b_bot_gripper_client.sendGoal(goal);
 
     // wait for the action to return
@@ -34,19 +39,17 @@ int main(int argc, char **argv)
 
     // ###############
 
-    // goal.position = 0.5;    // 255 to close, 0 to open the gripper
-    // b_bot_gripper_client.sendGoal(goal);
+    goal.position = 0.5;
+    b_bot_gripper_client.sendGoal(goal);
+    finished_before_timeout = b_bot_gripper_client.waitForResult(ros::Duration(5.0));
 
-    // // wait for the action to return
-    // finished_before_timeout = b_bot_gripper_client.waitForResult(ros::Duration(5.0));
-
-    // if (finished_before_timeout)
-    // {
-    //     actionlib::SimpleClientGoalState state = b_bot_gripper_client.getState();
-    //     ROS_INFO("Gripper-opening action finished: %s", state.toString().c_str());
-    // }
-    // else
-    //     ROS_INFO("Gripper-opening action did not finish before the time out.");    
+    if (finished_before_timeout)
+    {
+        actionlib::SimpleClientGoalState state = b_bot_gripper_client.getState();
+        ROS_INFO_STREAM("Gripper-opening action finished: " << state.toString());
+    }
+    else
+        ROS_INFO("Gripper-opening action did not finish before the time out.");    
 
     return 0;
 }
