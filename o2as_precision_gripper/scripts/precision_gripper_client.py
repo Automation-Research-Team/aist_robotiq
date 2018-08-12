272c64e4b8927bac327cf78fd2db5c0287fe06b9
Kaidi Nie <niekaidi92@yahoo.co.jp>
2018-08-12 17:26:47 +0900

    Add another service client call structure

--- o2as_precision_gripper/scripts/precision_gripper_client.py
+++ o2as_precision_gripper/scripts/precision_gripper_client.py
@@ -4,27 +4,20 @@ import sys
 import rospy
 from o2as_precision_gripper.srv import *
 
-# def precision_gripper_client(open_outer_gripper,close_outer_gripper,open_inner_gripper,close_inner_gripper):
-#     rospy.wait_for_service('precision_gripper')
-#     try:
-#         precision_gripper = rospy.ServiceProxy('precision_gripper',PrecisionGripperCommand)
-#         response1 = precision_gripper(open_outer_gripper,close_outer_gripper,open_inner_gripper,close_inner_gripper)
-#         return response1.success
-#     except rospy.ServiceException, e:
-#         print "Service call failed: %s"%e
-
 if __name__ == "__main__":
-    open_outer_gripper=True
-    close_outer_gripper=False
-    open_inner_gripper=False
-    close_inner_gripper=False
-    # rospy.loginfo(str(precision_gripper_client(open_outer_gripper,close_outer_gripper,open_inner_gripper,close_inner_gripper)))
-
+    
     rospy.wait_for_service('precision_gripper_command')
     print "kaidi"
     try:
         precision_gripper_client = rospy.ServiceProxy('precision_gripper_command',PrecisionGripperCommand)
-        response1 = precision_gripper_client(open_outer_gripper,close_outer_gripper,open_inner_gripper,close_inner_gripper)
+        
+        request = PrecisionGripperCommand()
+        request.open_outer_gripper_fully = True
+        # request.close_outer_gripper_fully = False
+        # request.open_inner_gripper_fully = False
+        # request.close_inner_gripper_fully = False
+        
+        response1 = precision_gripper_client(request)
         rospy.loginfo("response1.success = " + str(response1.success))
         print response1.success
     except rospy.ServiceException, e:
