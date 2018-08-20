6143c4bf9e824c5da7839c100ed53cd22fa55e9a
arayasu <kowa.yasushi@gmail.com>
2018-08-20 15:44:01 +0900

    Update fastener_gripper

--- o2as_fastener_gripper/src/dynamixel_controller.cpp
+++ o2as_fastener_gripper/src/dynamixel_controller.cpp
@@ -3,11 +3,8 @@
 
 using namespace dynamixel_controller;
 
-// サービスサーバ
-//コマンドの要請を受け取り、実行し結果を返す関数
 bool kowa::dynamixelCommandMsgCallback(o2as_fastener_gripper::DynamixelWriteCommand::Request &req, o2as_fastener_gripper::DynamixelWriteCommand::Response &res)
 {
-	//check a control table of a Dynamixel
 	uint8_t motor_id = req.motor_id;
 	std::string addr = req.addr_name;
 	int64_t value = req.value;
@@ -40,25 +37,23 @@ bool kowa::dynamixelReadMsgCallback(o2as_fastener_gripper::DynamixelReadState::R
 	return true;
 }
 
-//コンストラクタ
 kowa::kowa(void)
 {
-	int value = 10;                    // IDリスト要素数Max値
-	uint8_t id_cnt = 0;                // ID件数
-	id_list = new uint8_t[value];      // IDリスト
+	int value = 10;
+	uint8_t id_cnt = 0;
+	id_list = new uint8_t[value];
+	std::string connect_name;
+	int baundrate;
 
-	dynamixel_driver = new DynamixelDriver;  // Dynamixel Driver初期化
-	
-	dynamixel_driver->init("/dev/ttyUSB0", 1000000);   // Dynamixelのデバイス名、baud_rateを指定して初期化
+	ros::NodeHandle node_private("~");
+	node_private.param<std::string>("connect_name",connect_name,"");
+	node_private.param<int>("baundrate",baundrate,0);
 
-	printf("ok\n");
-	//Dynamixel検索
-	//第1引数：検索して見つかったDynamixelのIDを格納する配列
-	//第2引数：見つかったDynamixelの数
-	//第3引数：検索するDynamixel_IDの最大値
+	dynamixel_driver = new DynamixelDriver;
+	dynamixel_driver->init(connect_name.c_str(), baundrate);
+
 	if (dynamixel_driver->scan(id_list, &id_cnt, value))
 	{
-		//見つかれば情報出力
 		for(int i=0; i < id_cnt; i++)
 		{
 			printf("[ID] %u, [Model Name] %s, [VERSION] %.1f\n",
@@ -66,26 +61,20 @@ kowa::kowa(void)
 		}
 	}
 
-	//サービスサーバ宣言
-	//サービス名：dynamixel_command
-	//サービス要請があった場合、kowa::dynamixelCommandMsgCallback関数を実行する
-	//ros::ServiceServer dynamixel_command_server;
 	dynamixel_command_server_ = node_handle_.advertiseService("dynamixel_write_command", &kowa::dynamixelCommandMsgCallback,this);
 	dynamixel_info_server_ = node_handle_.advertiseService("dynamixel_read_state", &kowa::dynamixelReadMsgCallback,this);
 }
 
-//デストラクタ
 kowa::~kowa(void){}
-
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "dynamixel_controller");    // ノード名：single_dynamixel_controller
+	ros::init(argc, argv, "dynamixel_controller");
 
-	kowa test;  //クラス初期化
+	kowa test;
 
 	ros::Rate loop_rate(1000);
-
+	
 	ROS_INFO("dynamixel_controller START");
 
 	while (ros::ok())
