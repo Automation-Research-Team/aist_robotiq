4405b7d40dceb901ac823fb438afbcd8f16a5251
Felix von Drigalski <FvDrigalski@gmail.com>
2018-08-24 16:24:44 +0900

    Add assembly part frames & URDF generator
    Some part definitions (parts 2-6) are not complete because the mesh is not correct.

--- o2as_parts_description/README.md
+++ o2as_parts_description/README.md
@@ -1,8 +1,9 @@
 # o2as_parts_description
 
-This package includes URDF and mesh models of the parts used in WRC2018. The 
-models can be used for simulation. You can check the parts are correctly 
-spawned in the simulation environment by following the steps below.
+This package includes URDF and mesh models of the parts used in WRC2018. 
+The URDF files are generated automatically from the mesh files, and additional frames appended to it.
+The models can be used for simulation. You can check the parts are correctly 
+spawned in the Gazebo simulation environment by following the steps below.
 
 1. Launch simulated environment in Gazebo
     ```bash
@@ -19,7 +20,23 @@ spawned in the simulation environment by following the steps below.
     rosrun o2as_parts_description test_spawn_parts.py 16 50 4
     ```
 
+To view all the parts in Rviz, simply include "display_all_parts.srdf" in the scene.
+
+## Generating the URDF files
+
+The node `generate_urdf_from_meshes.py` takes the files in `/meshes` and the file `/urdf/templates/extra_frames.csv`, and generates the macro files in the `/urdf` directory. Files are overwritten, so be careful.
+
 ## Rules for item coordinate systems
 
-For items with an axis of rotation, the item coordinate system should be oriented with the z-axis pointing "forward" through the object, along the axis of symmetry. The start of the coordinate system should be the on the side of the center of gravity (on the side of the head of the screw).
+Meshes should be defined such that the coordinate system lies in an easily understood point on the object (in a corner, on the axis of symmetry at the end of the part etc).
 
+## How to define "mating" frames
+
+We plan to define frames to be used for planning motions on all of the parts. Thoughts:
+
+- Align the x-axis with the axis of rotation
+- Let the x-axis point inside holes
+- Let the x-axis point away from male parts (cylinders)
+- Put the frame at the surface level of surrounding material for holes, and at the tip of cylinders\screws
+- For male parts, the frame at the tip should be called _tip, the second one _inserted (the latter one will be fixed to the _hole frame)
+- For parts that are both rotation symmetric and can be turned over (washers, nuts, spacers...) only one frame should be in the center
---
+++ o2as_parts_description/scripts/generate_urdf_from_meshes.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+import csv
+import os
+
+import rospy
+import rospkg
+rp = rospkg.RosPack()
+
+# Read in the files
+filenames = os.listdir(os.path.join(rp.get_path("o2as_parts_description"), "meshes"))
+filenames_strip1 = []
+filenames_no_ext = []
+for name in filenames:
+    filenames_strip1.append(os.path.splitext(name)[0]) 
+for name in filenames_strip1: 
+    filenames_no_ext.append(os.path.splitext(name)[0])        # This removes the .vhacd from ".vhacd.dae" files
+partnames = list(sorted(set(filenames_no_ext)))   # Removes duplicates and sorts (because sets do not allow duplicate entries)
+
+# Read in the templates
+extra_joint_filename = os.path.join(rp.get_path("o2as_parts_description"), "urdf/templates", "extra_frames.csv")
+macro_template_filename = os.path.join(rp.get_path("o2as_parts_description"), "urdf/templates", "macro_template.urdf")
+spawn_template_filename = os.path.join(rp.get_path("o2as_parts_description"), "urdf/templates", "spawn_template.urdf")
+out_dir = os.path.join(rp.get_path("o2as_parts_description"), "urdf")
+
+f = open(macro_template_filename,'r')
+macro_template = f.read()
+f.close()
+f = open(spawn_template_filename,'r')
+spawn_template = f.read()
+f.close()
+extra_frames = []
+with open(extra_joint_filename, 'r') as f:
+  reader = csv.reader(f)
+  header = next(reader)
+  for row in reader:
+    row_stripped = []
+    for el in row:
+      row_stripped.append(el.strip())       # Removes whitespaces
+    extra_frames.append(row_stripped)
+
+# Write the spawn files
+# --- This is ignored for now
+
+# Write the macros
+for part_num, partname in enumerate(partnames):
+    outfile = open(os.path.join(out_dir, partname+"_macro.urdf"),'w+')
+    content = macro_template.replace("PARTNAME", partname)
+    mname = "assy_part_" + partname[0:2]
+    content = content.replace("MACRONAME", mname)     # This should be the number of the part
+
+    if int(partname[0:2]) in [1,2,3]:
+        content = content.replace("vhacd.dae", "stl")
+        content = content.replace("dae", "stl")
+
+    extra_frames_urdf = ""
+    for entry in extra_frames:
+        if int(entry[0]) == int(partname[0:2]):
+            new_joint = ""
+            new_joint +=  "    <joint name=\"${prefix}" + mname + "_LINK_NAME_joint\" type=\"fixed\"> \n"
+            new_joint +=  "      <parent link=\"${prefix}" + mname + "\"/> \n"
+            new_joint +=  "      <child link=\"${prefix}" + mname + "_LINK_NAME\"/> \n"
+            new_joint +=  "      <origin rpy=\"" + entry[2] + " " + entry[3] + " " + entry[4] + "\" xyz=\"" + entry[5] + " " + entry[6] + " " + entry[7] + " \"/> \n"
+            new_joint +=  "    </joint> \n"
+            new_joint +=  "    <link name=\"${prefix}" + mname + "_LINK_NAME\"/> \n"
+            new_joint +=  "    \n"
+            new_joint = new_joint.replace("LINK_NAME", entry[1])
+            extra_frames_urdf += new_joint
+    if extra_frames_urdf:
+        content = content.replace("<!-- #EXTRAFRAMES -->", extra_frames_urdf)
+    outfile.write(content)
+    rospy.loginfo("Wrote " + os.path.join(out_dir, partname+"_macro.urdf"))
+
+
+
+
+
