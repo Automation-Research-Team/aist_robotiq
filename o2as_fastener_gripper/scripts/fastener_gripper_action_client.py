5dd768f3fe4e2084adc5ca1686e52d6f1eb25be2
arayasu <kowa.yasushi@gmail.com>
2018-08-28 09:11:28 +0900

    Cleanup code of o2as_fastener_gripper

---
+++ o2as_fastener_gripper/scripts/fastener_gripper_action_client.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+import sys
+import rospy
+from std_msgs.msg import String
+from o2as_fastener_gripper.srv import *
+from o2as_fastener_gripper.msg import *
+
+import actionlib
+import actionlib_tutorials.msg
+
+class FastenerGripperController:
+    def fasten(self,name):
+        client = actionlib.SimpleActionClient('FastenerGripperAction', FastenerGripperControlAction)
+        client.wait_for_server()
+        goal = FastenerGripperControlGoal()
+
+        goal.gripper_name = name
+        goal.speed = 60
+
+        client.send_goal_and_wait(goal,rospy.Duration(10),rospy.Duration(10))
+        client.wait_for_result()
+        
+        return client.get_result()
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('fastener_gripper_controller_test')
+
+        controller = FastenerGripperController()
+
+        name_list = [
+            "gripper2",
+            "gripper3",
+        ]
+
+        rospy.sleep(3)
+
+        for name in name_list:
+            if not controller.fasten(name).control_result :
+                break
+
+        print('')
+
+    except rospy.ROSInterruptException:
+        print("program interrupted before completion", file=sys.stderr)
--- o2as_fastener_gripper/src/dynamixel_controller.cpp
+++ o2as_fastener_gripper/src/dynamixel_controller.cpp
@@ -1,81 +1,189 @@
+#include <fstream>
 #include "ros/ros.h"
 #include "o2as_fastener_gripper/dynamixel_controller.h"
 
 using namespace dynamixel_controller;
 
-bool kowa::dynamixelCommandMsgCallback(o2as_fastener_gripper::DynamixelWriteCommand::Request &req, o2as_fastener_gripper::DynamixelWriteCommand::Response &res)
+bool DynamixelControll::dynamixelCommandMsgCallback(o2as_fastener_gripper::DynamixelWriteCommand::Request &req, o2as_fastener_gripper::DynamixelWriteCommand::Response &res)
 {
 	uint8_t motor_id = req.motor_id;
-	std::string addr = req.addr_name;
+	std::string item = req.item_name;
 	int64_t value = req.value;
 
-	if (dynamixel_driver->writeRegister(motor_id, addr.c_str(), value))
-	{
-		ROS_DEBUG("OK\n");
-		res.comm_result = true;
-	}
-	else
-	{
-		 ROS_DEBUG("NG\n");
-		 res.comm_result = false;
-	}
-	return true;
-}
+	res.comm_result = true;
 
-bool kowa::dynamixelReadMsgCallback(o2as_fastener_gripper::DynamixelReadState::Request &req, o2as_fastener_gripper::DynamixelReadState::Response &res)
-{
-	int32_t item_status = 0;
-	if(dynamixel_driver->readRegister(req.motor_id, req.item_name.c_str(), &item_status))
+    int result_index = DynamixelControll::searchMotor(motor_id);
+	if (result_index == -1)
 	{
-		res.value = item_status;
-		res.comm_result = true;
-	}
-	else
-	{
+		ROS_ERROR("The specified Motor_ID (%d) can not be found.",motor_id);
 		res.comm_result = false;
 	}
-	return true;
+	else
+	{
+		if (dynamixel_driver[result_index]->writeRegister(motor_id, item.c_str(), value)) res.comm_result = true;
+	    else res.comm_result = false;
+	}
 }
 
-kowa::kowa(void)
+bool DynamixelControll::dynamixelReadMsgCallback(o2as_fastener_gripper::DynamixelReadState::Request &req, o2as_fastener_gripper::DynamixelReadState::Response &res)
 {
-	int value = 10;
-	uint8_t id_cnt = 0;
-	id_list = new uint8_t[value];
-	std::string connect_name;
-	int baundrate;
+	uint8_t motor_id = req.motor_id;
+	int32_t item_status = 0;
 
-	ros::NodeHandle node_private("~");
-	node_private.param<std::string>("connect_name",connect_name,"");
-	node_private.param<int>("baundrate",baundrate,0);
-
-	dynamixel_driver = new DynamixelDriver;
-	dynamixel_driver->init(connect_name.c_str(), baundrate);
-
-	if (dynamixel_driver->scan(id_list, &id_cnt, value))
+    int result_index = DynamixelControll::searchMotor(motor_id);
+	if (result_index == -1)
 	{
-		for(int i=0; i < id_cnt; i++)
+		ROS_ERROR("The specified Motor_ID (%d) can not be found.",motor_id);
+		res.comm_result = false;
+	}
+	else
+	{
+		if (dynamixel_driver[result_index]->readRegister(motor_id, req.item_name.c_str(), &item_status))
 		{
-			printf("[ID] %u, [Model Name] %s, [VERSION] %.1f\n",
-							 id_list[i], dynamixel_driver->getModelName(id_list[i]), dynamixel_driver->getProtocolVersion());
+			res.value = item_status;
+			res.comm_result = true;
+		}
+	    else res.comm_result = false;
+	}
+}
+
+int DynamixelControll::searchMotor(uint8_t motor_id)
+{
+	std::vector<std::string> split_list;
+
+	for(int index=0; index < u2d2_connect_id_list.size(); index++)
+	{
+		if (u2d2_connect_id_list[index] != "")
+		{
+			split_list = DynamixelControll::StringSplit(u2d2_connect_id_list[index],',');
+			for(int i=0; i<split_list.size(); i++) if (std::stoi(split_list[i]) == motor_id) return index;
 		}
 	}
 
-	dynamixel_command_server_ = node_handle_.advertiseService("dynamixel_write_command", &kowa::dynamixelCommandMsgCallback,this);
-	dynamixel_info_server_ = node_handle_.advertiseService("dynamixel_read_state", &kowa::dynamixelReadMsgCallback,this);
+	return -1;
 }
 
-kowa::~kowa(void){}
+bool DynamixelControll::initMotor(int32_t index, uint8_t motor_id, bool baudrate_flag)
+{
+	if (baudrate_flag)
+	{
+		if (! dynamixel_driver[index]->writeRegister(motor_id, "Baud_Rate", 1000000))
+	    {
+	    	ROS_ERROR("The value of Baud_Rate could not be set to 1000000.");
+	    	return false;
+	    }
+	}
+
+	if (! dynamixel_driver[index]->writeRegister(motor_id, "Torque_Enable", 0))
+	{
+		ROS_ERROR("The value of Torque_Enable could not be set to 0.");
+		return false;
+	}
+	if (! dynamixel_driver[index]->writeRegister(motor_id, "Control_Mode", 1))
+	{
+		ROS_ERROR("The value of Control_Mode could not be set to 1.");
+		return false;
+	}
+	if (! dynamixel_driver[index]->writeRegister(motor_id, "Torque_Limit", 1023))
+	{
+		ROS_ERROR("The value of Torque_Limit could not be set to 1023.");
+		return false;
+	}
+
+	return true;
+}
+
+DynamixelControll::DynamixelControll(void)
+{
+	int max_access = 0;
+    int value = 10;
+	bool exist_flag = true;
+	std::string access;
+	uint8_t id_cnt = 0;
+	id_list = new uint8_t[value];
+	
+	// check access point
+    ros::NodeHandle nh("~");
+	if (! nh.getParam("max_access", max_access))
+	{
+		ROS_ERROR("No exist param. (max_access)");
+		return;
+	}
+
+	for(int no=1; no <= max_access; no++){
+		access = "access_" + std::to_string(no);
+		if (nh.getParam(access, access)) access_point_list.push_back(access);
+		else
+		{
+			ROS_ERROR("No exist param. (%s)", access.c_str());
+			exist_flag = false;
+		}
+	}
+	if (! exist_flag) return;
+
+
+    //init and serach XL-320
+	for(int index=0; index < access_point_list.size(); index++)
+	{
+		std::ifstream ifs(access_point_list[index]);
+	    if (ifs.is_open())
+	    {
+			u2d2_connect_id_list.push_back("");
+			dynamixel_driver[index] = new DynamixelDriver;
+	        for(int n=0; n<4; n++)
+			{
+				exist_flag = false;
+				dynamixel_driver[index]->init(access_point_list[index].c_str(), baudrate_list[n]);
+			
+			    if (dynamixel_driver[index]->scan(id_list, &id_cnt, value))
+			    {
+			    	for(int i=0; i < id_cnt; i++)
+			    	{
+			    		if (i==0) ROS_INFO("Connection wsa found. (%s)" , access_point_list[index].c_str());
+			    		ROS_INFO("[ID] %u, [Model Name] %s, [VERSION] %.1f",
+			    						id_list[i], dynamixel_driver[index]->getModelName(id_list[i]), dynamixel_driver[index]->getProtocolVersion());
+			    		
+			    		//Initial Setting(XL320)
+						if (baudrate_list[n] != 1000000) baudrate_flag = true;
+						else baudrate_flag = false;
+
+			    		if (DynamixelControll::initMotor(index, id_list[i], baudrate_flag))
+			    		{
+			    			if (u2d2_connect_id_list[index] == "") u2d2_connect_id_list[index] = std::to_string(id_list[i]);
+			    			else u2d2_connect_id_list[index] = u2d2_connect_id_list[index] + "," + std::to_string(id_list[i]);
+			    		}
+			    	}
+					exist_flag = true;
+					break;
+			    }
+			}
+			if (! exist_flag)
+			{
+				ROS_INFO("Connection wsa found. (%s)" , access_point_list[index].c_str());
+				ROS_ERROR("Can not find XL-320.");
+			}
+	    }
+	    else
+		{
+			ROS_WARN("No exist File (%s)",access_point_list[index].c_str());
+			access_point_list[index] = "";
+		}
+	}
+	
+	dynamixel_command_server_ = node_handle_.advertiseService("dynamixel_write_command", &DynamixelControll::dynamixelCommandMsgCallback,this);
+	dynamixel_info_server_ = node_handle_.advertiseService("dynamixel_read_state", &DynamixelControll::dynamixelReadMsgCallback,this);
+}
+
+DynamixelControll::~DynamixelControll(void){}
+
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "dynamixel_controller");
 
-	kowa test;
+	DynamixelControll controller;
 
 	ros::Rate loop_rate(1000);
-	
-	ROS_INFO("dynamixel_controller START");
 
 	while (ros::ok())
 	{
