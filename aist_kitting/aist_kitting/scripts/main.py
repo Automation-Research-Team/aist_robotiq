764b58435f0f00829edfe11b6b814e66e60e2c22
Felix von Drigalski <FvDrigalski@gmail.com>
2018-08-30 13:20:55 +0900

    Add TF transformations examples

--- aist_kitting/aist_kitting/scripts/main.py
+++ aist_kitting/aist_kitting/scripts/main.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import rospy
 import tf
+import tf_conversions
+from math import *
 
 from aist_kitting_msgs.srv import *
 # from o2as_usb_relay.srv import SetPower
@@ -79,6 +81,44 @@ def suction_test():
     rospy.loginfo(res_suction_trigger.message)
     if res_suction_trigger.success:
         rospy.sleep(3)
+
+def phoxi_transformation_examples():
+    # original matrix
+    T_phoxi = np.array([
+        [-0.045899001285, 0.902472945015, -0.428294133975, 0.594556671213511],
+        [0.984380843543, -0.032086403336, -0.173103488082, 0.139281989152648 ],
+        [-0.169963633010, -0.429549818109, -0.886904345020, 1.467133761064637 ],
+        [0.000000000000, 0.000000000000, 0.000000000000, 1.000000000000 ]
+    ])
+
+    # https://github.com/ros/geometry/blob/hydro-devel/tf/src/tf/transformations.py
+    # Example code for getting rpy angles and translation from the 4x4 homogenous matrix via TF
+    scale, shear, angles, trans, persp = tf.transformations.decompose_matrix(T_phoxi)
+    print(angles)
+    print(trans)
+
+    rpy = tf.transformations.euler_from_matrix(T_phoxi)
+    xyz = [T_phoxi[0,3], T_phoxi[1,3], T_phoxi[2,3]]
+    print(rpy)
+    print(xyz)
+
+    # T_phoxi is the transformation to the camera frame
+    # The transformation to be entered into the URDF is to the camera base frame
+
+    # One possibility: Take the transformation between camera frame and camera base_frame from the URDF,
+    # and add it to the transformation here manually.
+
+    rpy_camera_frame_to_base = [pi/2, pi/2, pi/2]
+    xyz_camera_frame_to_base = [1, 2, 3]
+    T_camera_frame_to_base = tf.transformations.compose_matrix(scale=None, shear=None, angles=rpy_camera_frame_to_base, 
+                        translate=xyz_camera_frame_to_base, perspective=None)
+
+    T_phoxi_base = tf.transformations.concatenate_matrices(T_phoxi, T_camera_frame_to_base)
+    # Now rpy and xyz can be extracted for the T_phoxi_base transformation and used in the scene URDF,
+    # as the transformation from world to camera base.
+    # NOTE: The signs and multiplications in the code above may have to be inverted.
+
+    return
 
 def transform_phoxi_to_world(pose):
     # original matrix
@@ -200,8 +240,8 @@ def main():
 
 if __name__ == '__main__':
     rospy.init_node("aist_kitting_demo")
-
-    main()
+    phoxi_transformation_examples
+    # main()
 
     # pick_test()
     # move_named_pose_test()
