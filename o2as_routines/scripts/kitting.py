3d12803734161e8e3b484187ce8643370f67bcaf
Yuma Hijioka <yuma.hijioka@aist.go.jp>
2018-09-04 10:36:51 +0900

    Add first demon-simulation pick-and-place.

--- o2as_routines/scripts/kitting.py
+++ o2as_routines/scripts/kitting.py
@@ -11,6 +11,7 @@ from math import pi
 from o2as_msgs.srv import *
 import actionlib
 import o2as_msgs.msg
+from o2as_usb_relay.srv import *
 
 from o2as_routines.base import O2ASBaseRoutines
 
@@ -22,6 +23,7 @@ class KittingClass(O2ASBaseRoutines):
   def __init__(self):
     super(KittingClass, self).__init__()
     self.set_up_item_parameters()
+    self.set_up_end_effector()
     rospy.sleep(.5)
 
   def set_up_item_parameters(self):
@@ -29,12 +31,17 @@ class KittingClass(O2ASBaseRoutines):
     downward_orientation = geometry_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(0, pi/2, 0))
     # 
     
+  def set_up_end_effector(self):
+    self.suction = rospy.ServiceProxy("o2as_usb_relay_server/set_power", SetPower)
 
   ################ ----- Routines  
   ################ 
   ################ 
 
-  def pick(self, robot_name, object_pose, grasp_height, speed_fast, speed_slow, approach_height=0.03):
+  def switch_suction(self, on=False):
+    return self.suction(1, on)
+
+  def pick(self, robot_name, object_pose, speed_fast, speed_slow, approach_height=0.03):
 
     self.publish_marker(object_pose, "aist_vision_result")
     if robot_name=="b_bot":
@@ -43,36 +50,67 @@ class KittingClass(O2ASBaseRoutines):
     rospy.loginfo("Going above object to pick")
     approach_pose = geometry_msgs.msg.PoseStamped()
     approach_pose = copy.deepcopy(object_pose)
-    approach_pose.pose.position.z += 0.03
+    approach_pose.pose.position.z += approach_height
     approach_pose.pose.orientation.x = -0.5
     approach_pose.pose.orientation.y = 0.5
     approach_pose.pose.orientation.z = 0.5
     approach_pose.pose.orientation.w = 0.5
-    res = self.go_to_pose_goal(robot_name, approach_pose, speed=speed_fast)
+    self.go_to_pose_goal(robot_name, approach_pose, speed=speed_fast)
 
     rospy.loginfo("Moving down to object")
     self.go_to_pose_goal(robot_name, object_pose, speed=speed_slow, high_precision=True)
-
-    rospy.loginfo("suction on")
+    
+    rospy.loginfo("Picking up on suction")
+    # self.switch_suction(True)
     rospy.sleep(2)
 
     rospy.loginfo("Going back up")
     self.go_to_pose_goal(robot_name, approach_pose, speed=speed_fast)
+
+  def place(self, robot_name, tray_id, place_height, speed_fast, speed_slow, approach_height=0.05):
+
+    goal_pose = geometry_msgs.msg.PoseStamped()
+    goal_pose.header.frame_id = tray_id
+    goal_pose.pose.orientation.x = -0.5
+    goal_pose.pose.orientation.y = 0.5
+    goal_pose.pose.orientation.z = 0.5
+    goal_pose.pose.orientation.w = 0.5
+    goal_pose.pose.position.z = approach_height
+    self.go_to_pose_goal(robot_name, goal_pose, speed=speed_fast)
+
+    rospy.loginfo("Moving down to object")
+    goal_pose.pose.position.z = place_height
+    self.go_to_pose_goal(robot_name, goal_pose, speed=speed_slow, high_precision=True)
+    rospy.loginfo("Place down from suction")
+    # self.switch_suction(False)
+    rospy.sleep(2)
+
+    rospy.loginfo("Going back up")
+    goal_pose.pose.position.z += approach_height
+    self.go_to_pose_goal(robot_name, goal_pose, speed=speed_fast)
+
+  def pick_and_place_demo(self):
+    object_pose = geometry_msgs.msg.PoseStamped()
+    object_pose.header.frame_id = "set1_bin3_1"
+    object_pose.pose.position.z = 0.01
+    object_pose.pose.orientation = geometry_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(pi/4, pi/4, 0))
+    rospy.loginfo("Picing up an demo object.")
+    self.pick("b_bot", object_pose, 1.0, 0.05)
+
+    tray_id = "set3_tray_1_partition_2"
+    rospy.loginfo("Place down an demo object on tray partition.")
+    self.place("b_bot", tray_id, 0.005, 1.0, 0.05)
 
   def kitting_task(self):
     self.go_to_named_pose("home", "c_bot")
     self.go_to_named_pose("home", "b_bot")
     self.go_to_named_pose("home", "a_bot")
 
+    self.pick_and_place_demo()
 
-    ps = geometry_msgs.msg.PoseStamped()
-    ps.header.frame_id = "set1_bin2_1"
-    ps.pose.position.z = 0.05
-    ps.pose.orientation = geometry_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(pi/4, pi/4, 0))
-    
-    # ps.pose.orientation = geometry_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(0, 0, 0))
-    self.pick("b_bot", ps, ps.pose.position.z, 1.0, 0.05, ps.pose.position.z+0.03)
-    # self.publish_marker(ps, "aist_vision_result")
+    self.go_to_named_pose("home", "c_bot")
+    self.go_to_named_pose("home", "b_bot")
+    self.go_to_named_pose("home", "a_bot")
 
     # TODO
 
