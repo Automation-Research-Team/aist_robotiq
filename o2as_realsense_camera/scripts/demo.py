92cfaedce7bafdb2b4b7bcae1f74d5103210e7d4
Yoshiya Shibata <shivatti777@gmail.com>
2018-09-05 11:23:29 +0900

    Add get_frame and dump_frame service to o2as_realsense_camera

--- o2as_realsense_camera/scripts/demo.py
+++ o2as_realsense_camera/scripts/demo.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python
 import rospy
+import cv2
+from cv_bridge import CvBridge
 from o2as_realsense_camera.client import *
 
 if __name__ == "__main__":
-    rospy.init_node('camera_client', anonymous=True)
+    rospy.init_node('realsense_camera_demo', anonymous=True, log_level=rospy.DEBUG)
 
-    # get params
-    setting_filename = rospy.get_param("~setting_filename")
-    pcloud_filename = rospy.get_param("~pcloud_filename")
-    image_filename = rospy.get_param("~image_filename")
+    # connect to the camera server using client class
+    camera_node_name = rospy.get_param("~camera_node_name")
+    camera = RealSenseCameraClient(camera_node_name)
+    bridge = CvBridge()
 
-    # capture image from camera and save them to file continuously
-    camera = RealSenseCameraClient()
-    r = rospy.Rate(60)
+    cv2.namedWindow('color_image', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('depth_image', cv2.WINDOW_NORMAL)
+    
     try:
-        camera.connect()
-        camera.save_camera_info(setting_filename)
         while not rospy.is_shutdown():
-            rospy.loginfo("save frame for cad matching")
-            res = camera.save_frame_for_cad_matching(pcloud_filename, image_filename)
-            r.sleep()
+            resp = camera.get_frame(dump=False, publish=True)
+            resp = camera.get_frame(dump=True, publish=True)
+
+            # display captured frame
+            cv_color_image = bridge.imgmsg_to_cv2(resp.color_image, desired_encoding="passthrough")
+            cv_depth_image = bridge.imgmsg_to_cv2(resp.depth_image, desired_encoding="passthrough")
+            rospy.logdebug("image size: " + str(cv_color_image.shape))
+            cv2.imshow('color_image', cv_color_image)
+            cv2.imshow('depth_image', cv_depth_image)
+            cv2.waitKey(1)
+
     except rospy.ServiceException, e:
         rospy.logerr("Service call failed: %s" % e)
+
+    cv2.destroyAllWindows()
