f1dd71e89cf28b9b764d32e3628871b4d4e4653a
Taku Yoshioka <yoshioka@sensetime.jp>
2018-09-19 13:35:45 +0900

    WIP: Keep callback functions in a vector

--- o2as_debug_monitor/src/o2as_debug_monitor.cpp
+++ o2as_debug_monitor/src/o2as_debug_monitor.cpp
@@ -11,6 +11,8 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 
+using ImageCallback = boost::function<void(const sensor_msgs::ImageConstPtr&)>;
+
 namespace o2as_debug_monitor
 {
   // Adopted from team-NAIST-Panasonic ARC repository
@@ -33,54 +35,60 @@ namespace o2as_debug_monitor
     return cv_ptr;
   }
 
+  boost::function<void(const sensor_msgs::ImageConstPtr&)>
+    getCallbackForImage(cv::Rect rect, cv::Mat& monitor_) {
+    // This function returns callback function
+    return [=](const sensor_msgs::ImageConstPtr &msg) {
+      // Show window at the first time
+      if (!cvGetWindowHandle("Monitor")) {
+        cv::namedWindow("Monitor", cv::WINDOW_NORMAL);
+        cv::moveWindow("Monitor", 128, 128);
+      }
+
+      ROS_INFO("Image callback was invoked.");
+
+      // Convert sent message into cv::Mat
+      cv::Mat img = convert2OpenCV(
+        *msg, sensor_msgs::image_encodings::RGB8
+      ) -> image;
+
+      // Resize the image and put it to the buffer
+      cv::Size size = cv::Size(rect.width, rect.height);
+      cv::Mat resized;
+      cv::resize(img, resized, size, 0, 0, cv::INTER_CUBIC);
+      resized.copyTo(monitor_(rect));
+
+      // Copy the image buffer in the window
+        cv::imshow("Monitor", monitor_);
+        cv::waitKey(3);
+    };
+  }
+
   class Monitor
   {
     public:
       Monitor(ros::NodeHandle &nh)
       {
+        // Initialize shared objects in this class
         image_transport::ImageTransport it_(nh);
+        monitor_ = cv::Mat(480, 640, CV_8UC3, cv::Scalar(255, 0, 0));
+
+        // Append callback functions
+        imgcbs.push_back(
+          getCallbackForImage(cv::Rect(0, 0, 320, 240), monitor_)
+        );
         sub_rs_ = it_.subscribe("/b_bot_camera/rgb/image_raw", 1,
-                                &Monitor::realsenseCallback, this);
+                                imgcbs.back());
       }
 
     private:
       image_transport::Subscriber sub_rs_;
 
+//      boost::function<void(const sensor_msgs::ImageConstPtr&)> callback;
+      std::vector<ImageCallback> imgcbs;
+
       cv::Mat monitor_;
       cv::Mat realsense_img_;
-
-      void realsenseCallback(const sensor_msgs::ImageConstPtr &msg)
-      {
-        // Show window at the first time
-        if (!cvGetWindowHandle("Monitor")) {
-          cv::namedWindow("Monitor", cv::WINDOW_NORMAL);
-          cv::moveWindow("Monitor", 128, 128);
-        }
-
-        // Create image buffer
-        monitor_ = cv::Mat(480, 640, CV_8UC3, cv::Scalar(255, 0, 0));
-
-//        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(
-//          msg, sensor_msgs::image_encodings::BGR8
-//        );
-
-
-
-        // Copy realsense image to the image buffer
-        realsense_img_ = convert2OpenCV(
-          *msg, sensor_msgs::image_encodings::RGB8) -> image;
-        cv::Size size = cv::Size(320, 240);
-        cv::Rect rect = cv::Rect(0, 0, 320, 240);
-        cv::Mat resized;
-        cv::resize(realsense_img_, resized, size, 0, 0, cv::INTER_CUBIC);
-
-        //realsense_img_.copyTo(monitor_(rect));
-        resized.copyTo(monitor_(rect));
-
-        // Copy the image buffer in the window
-        cv::imshow("Monitor", monitor_);
-        cv::waitKey(3);
-      }
   };
 
   class O2asDebugMonitor : public nodelet::Nodelet
