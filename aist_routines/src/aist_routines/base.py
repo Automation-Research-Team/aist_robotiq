9a51931092805673652ec76e33529a4e39412446
Yuma Hijioka <yuma.hijioka@aist.go.jp>
2018-12-14 14:19:15 +0900

    Modify that AISTBaseRoutines allows used it by multiple nodes
    Refere to '1.2 Code explained' from below URL.
    http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber(python)

--- aist_routines/src/aist_routines/base.py
+++ aist_routines/src/aist_routines/base.py
@@ -77,7 +77,7 @@ def all_close(goal, actual, tolerance):
 class AISTBaseRoutines(object):
     def __init__(self):
         moveit_commander.roscpp_initialize(sys.argv)
-        rospy.init_node("kitting_task", anonymous=False)
+        rospy.init_node("kitting_task", anonymous=True)
 
         self.robots = moveit_commander.RobotCommander()
         self.planning_scene = moveit_commander.PlanningSceneInterface()
