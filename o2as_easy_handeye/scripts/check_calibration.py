e01548c45429b230ddf5748b2ce0f4d5b4882dc3
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2019-04-19 08:48:38 +0900

    Add ur5e_gazebo.launch for invoking simulator of UR5e robots.

--- o2as_easy_handeye/scripts/check_calibration.py
+++ o2as_easy_handeye/scripts/check_calibration.py
@@ -17,140 +17,159 @@ from o2as_routines.base import O2ASBaseRoutines
 from aist_routines.base import AISTBaseRoutines
 from math import radians, degrees
 
+
 ######################################################################
 #  global functions                                                  #
 ######################################################################
 def format_pose(pose):
-  rpy = map(degrees, tfs.euler_from_quaternion(
-                        [pose.orientation.w, pose.orientation.x,
-                         pose.orientation.y, pose.orientation.z]))
-  return "[{:.4f}, {:.4f}, {:.4f}; {:.2f}, {:.2f}. {:.2f}]".format(
-    pose.position.x, pose.position.y, pose.position.z, rpy[0], rpy[1], rpy[2])
+    rpy = map(
+        degrees,
+        tfs.euler_from_quaternion([
+            pose.orientation.w, pose.orientation.x, pose.orientation.y,
+            pose.orientation.z
+        ]))
+    return "[{:.4f}, {:.4f}, {:.4f}; {:.2f}, {:.2f}. {:.2f}]".format(
+        pose.position.x, pose.position.y, pose.position.z, rpy[0], rpy[1],
+        rpy[2])
 
 
 ######################################################################
 #  class VisitRoutines                                               #
 ######################################################################
 class VisitRoutines:
-  """Wrapper of MoveGroupCommander specific for this script"""
-  def __init__(self, routines, camera_name, robot_name):
-    self.routines = routines
+    """Wrapper of MoveGroupCommander specific for this script"""
 
-    cs = "/{}/".format(camera_name)
-    self.start_acquisition = rospy.ServiceProxy(cs + "start_acquisition",
-                                                Trigger)
-    self.stop_acquisition  = rospy.ServiceProxy(cs + "stop_acquisition",
-                                                Trigger)
+    def __init__(self, routines, camera_name, robot_name):
+        self.routines = routines
 
-    # Initialize `moveit_commander`
-    self.group_name = robot_name
-    group = self.routines.groups[self.group_name]
+        cs = "/{}/".format(camera_name)
+        self.start_acquisition = rospy.ServiceProxy(cs + "start_acquisition",
+                                                    Trigger)
+        self.stop_acquisition = rospy.ServiceProxy(cs + "stop_acquisition",
+                                                   Trigger)
 
-    # Set `_ee_link` as end effector wrt `_base_link` of the robot
-    group.set_pose_reference_frame("workspace_center")
-    if robot_name == 'b_bot':
-      group.set_end_effector_link(robot_name + "_single_suction_gripper_pad_link")
-    else:
-      group.set_end_effector_link(robot_name + "_robotiq_85_tip_link")
+        # Initialize `moveit_commander`
+        self.group_name = robot_name
+        group = self.routines.groups[self.group_name]
 
-    # Logging
-    print("==== Planning frame:       %s" % group.get_planning_frame())
-    print("==== Pose reference frame: %s" % group.get_pose_reference_frame())
-    print("==== End effector link:    %s" % group.get_end_effector_link())
+        # Set `_ee_link` as end effector wrt `_base_link` of the robot
+        group.set_pose_reference_frame("workspace_center")
+        if robot_name == 'b_bot':
+            group.set_end_effector_link(robot_name +
+                                        "_single_suction_gripper_pad_link")
+        else:
+            group.set_end_effector_link(robot_name + "_robotiq_85_tip_link")
 
+        # Logging
+        print("==== Planning frame:       %s" % group.get_planning_frame())
+        print("==== Pose reference frame: %s" %
+              group.get_pose_reference_frame())
+        print("==== End effector link:    %s" % group.get_end_effector_link())
 
-  def move(self, speed):
-    self.start_acquisition()
-    position = rospy.wait_for_message("/aruco_tracker/position",
-                                      geometry_msgs.msg.Vector3Stamped, 10)
-    self.stop_acquisition()
-
-    group = self.routines.groups[self.group_name]
-    poseStamped = geometry_msgs.msg.PoseStamped()
-    poseStamped.header.frame_id = group.get_pose_reference_frame()
-    poseStamped.pose.position.x = position.vector.x
-    poseStamped.pose.position.y = position.vector.y
-    poseStamped.pose.position.z = position.vector.z + 0.05
-    poseStamped.pose.orientation \
-      = geometry_msgs.msg.Quaternion(
-        *tfs.quaternion_from_euler(radians(0), radians(90), radians(-90)))
-    [all_close, move_success] \
-        = self.routines.go_to_pose_goal(
-              self.group_name, poseStamped, speed,
-              end_effector_link=group.get_end_effector_link(),
-              move_lin=False)
-    rospy.sleep(1)
-
-    poseStamped.pose.position.z = position.vector.z
-    print("     move to " + format_pose(poseStamped.pose))
-    [all_close, move_success] \
-        = self.routines.go_to_pose_goal(
-              self.group_name, poseStamped, speed,
-              end_effector_link=group.get_end_effector_link(),
-              move_lin=False)
-
-    poseReached = self.routines.listener.transformPose(
-                        group.get_pose_reference_frame(),
-                        group.get_current_pose())
-    print("  reached to " + format_pose(poseReached.pose))
-
-
-  def go_home(self):
-    self.routines.go_to_named_pose("home", self.group_name)
-
-
-  def run(self, speed):
-    self.stop_acquisition()
-    self.go_home()
-
-    while True:
-      try:
-        key = raw_input(">> ")
-        if key == 'q':
-          break
-        self.move(speed)
-      except Exception as ex:
+    def move(self, speed):
+        self.start_acquisition()
+        position = rospy.wait_for_message("/aruco_tracker/position",
+                                          geometry_msgs.msg.Vector3Stamped, 10)
         self.stop_acquisition()
-        print ex.message
-      except rospy.ROSInterruptException:
-        return
-      except KeyboardInterrupt:
-        return
 
-    self.go_home()
+        group = self.routines.groups[self.group_name]
+        poseStamped = geometry_msgs.msg.PoseStamped()
+        poseStamped.header.frame_id = group.get_pose_reference_frame()
+        poseStamped.pose.position.x = position.vector.x
+        poseStamped.pose.position.y = position.vector.y
+        poseStamped.pose.position.z = position.vector.z + 0.05
+        poseStamped.pose.orientation \
+          = geometry_msgs.msg.Quaternion(
+            *tfs.quaternion_from_euler(radians(0), radians(90), radians(-90)))
+        [all_close, move_success] \
+            = self.routines.go_to_pose_goal(
+                  self.group_name, poseStamped, speed,
+                  end_effector_link=group.get_end_effector_link(),
+                  move_lin=False)
+        rospy.sleep(1)
+
+        poseStamped.pose.position.z = position.vector.z
+        print("     move to " + format_pose(poseStamped.pose))
+        [all_close, move_success] \
+            = self.routines.go_to_pose_goal(
+                  self.group_name, poseStamped, speed,
+                  end_effector_link=group.get_end_effector_link(),
+                  move_lin=False)
+
+        poseReached = self.routines.listener.transformPose(
+            group.get_pose_reference_frame(), group.get_current_pose())
+        print("  reached to " + format_pose(poseReached.pose))
+
+    def go_home(self):
+        self.routines.go_to_named_pose("home", self.group_name)
+
+    def run(self, speed):
+        self.stop_acquisition()
+        self.go_home()
+
+        while True:
+            try:
+                key = raw_input(">> ")
+                if key == 'q':
+                    break
+                self.move(speed)
+            except Exception as ex:
+                self.stop_acquisition()
+                print ex.message
+            except rospy.ROSInterruptException:
+                return
+            except KeyboardInterrupt:
+                return
+
+        self.go_home()
 
 
 ######################################################################
 #  global functions                                                  #
 ######################################################################
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description='Check hand-eye calibration')
-  parser.add_argument('-C', '--config',
-                      action='store', nargs='?',
-                      default='aist', type=str, choices=None,
-                      help='configuration name', metavar=None)
-  parser.add_argument('-c', '--camera_name',
-                      action='store', nargs='?',
-                      default='a_phoxi_m_camera', type=str, choices=None,
-                      help='camera name', metavar=None)
-  parser.add_argument('-r', '--robot_name',
-                      action='store', nargs='?',
-                      default='b_bot', type=str, choices=None,
-                      help='robot name', metavar=None)
+    parser = argparse.ArgumentParser(description='Check hand-eye calibration')
+    parser.add_argument('-C',
+                        '--config',
+                        action='store',
+                        nargs='?',
+                        default='aist',
+                        type=str,
+                        choices=None,
+                        help='configuration name',
+                        metavar=None)
+    parser.add_argument('-c',
+                        '--camera_name',
+                        action='store',
+                        nargs='?',
+                        default='a_phoxi_m_camera',
+                        type=str,
+                        choices=None,
+                        help='camera name',
+                        metavar=None)
+    parser.add_argument('-r',
+                        '--robot_name',
+                        action='store',
+                        nargs='?',
+                        default='b_bot',
+                        type=str,
+                        choices=None,
+                        help='robot name',
+                        metavar=None)
 
-  args = parser.parse_args()
-  print(args)
+    args = parser.parse_args()
+    print(args)
 
-  if args.config == 'aist':
-    base_routines = AISTBaseRoutines()
-  else:
-    base_routines = O2ASBaseRoutines()
-  camera_name = args.camera_name
-  robot_name  = args.robot_name
+    if args.config == 'aist':
+        base_routines = AISTBaseRoutines()
+    else:
+        base_routines = O2ASBaseRoutines()
+    camera_name = args.camera_name
+    robot_name = args.robot_name
 
-  assert(camera_name in {"a_phoxi_m_camera", "a_bot_camera"})
-  assert(robot_name  in {"a_bot", "b_bot", "c_bot"})
+    assert (camera_name in {"a_phoxi_m_camera", "a_bot_camera"})
+    assert (robot_name in {"a_bot", "b_bot", "c_bot"})
 
-  routines = VisitRoutines(base_routines, camera_name, robot_name)
-  speed    = 0.05
-  routines.run(speed)
+    routines = VisitRoutines(base_routines, camera_name, robot_name)
+    speed = 0.05
+    routines.run(speed)
--- o2as_easy_handeye/scripts/publish.py
+++ o2as_easy_handeye/scripts/publish.py
@@ -6,94 +6,100 @@ from geometry_msgs.msg import Transform
 from math import radians, degrees
 from easy_handeye.handeye_calibration import HandeyeCalibration
 
+
 #########################################################################
 #  local functions                                                      #
 #########################################################################
 def get_transform(dst_frm, src_frm):
-  listener = TransformListener()
-  now      = rospy.Time.now()
-  listener.waitForTransform(dst_frm, src_frm, now, rospy.Duration(10))
-  return listener.lookupTransform(dst_frm, src_frm, now)
+    listener = TransformListener()
+    now = rospy.Time.now()
+    listener.waitForTransform(dst_frm, src_frm, now, rospy.Duration(10))
+    return listener.lookupTransform(dst_frm, src_frm, now)
 
 
 def get_estimated_transformation():
-  calib = HandeyeCalibration()
-  calib.from_file()
+    calib = HandeyeCalibration()
+    calib.from_file()
 
-  # Set ID of optical frame of the camera.
-  calib.transformation.child_frame_id = rospy.get_param('tracking_base_frame')
+    # Set ID of optical frame of the camera.
+    calib.transformation.child_frame_id = rospy.get_param(
+        'tracking_base_frame')
 
-  # Set ID of effector/base frame of the robot.
-  calib.transformation.header.frame_id = \
-    rospy.get_param('robot_effector_frame') if calib.eye_on_hand else \
-    rospy.get_param('robot_base_frame')
+    # Set ID of effector/base frame of the robot.
+    calib.transformation.header.frame_id = \
+      rospy.get_param('robot_effector_frame') if calib.eye_on_hand else \
+      rospy.get_param('robot_base_frame')
 
-  rospy.loginfo('loading calibration parameters into namespace {}'
-                .format(rospy.get_namespace()))
-  calib.to_parameters()
+    rospy.loginfo('loading calibration parameters into namespace {}'.format(
+        rospy.get_namespace()))
+    calib.to_parameters()
 
-  root_frame = calib.transformation.header.frame_id if calib.eye_on_hand else \
-               'o2as_ground'
+    root_frame = calib.transformation.header.frame_id if calib.eye_on_hand else \
+                 'o2as_ground'
 
-  return calib.transformation, root_frame
+    return calib.transformation, root_frame
 
 
 def print_mat(mat):
-  xyz = tfs.translation_from_matrix(mat)
-  rpy = map(degrees, tfs.euler_from_matrix(mat))
-  print '<origin xyz="{0[0]} {0[1]} {0[2]}" rpy="${{{1[0]}*pi/180}} ${{{1[1]}*pi/180}} ${{{1[2]}*pi/180}}"/>'.format(xyz, rpy)
-  q   = tfs.quaternion_from_matrix(mat)
-  print (xyz, q)
+    xyz = tfs.translation_from_matrix(mat)
+    rpy = map(degrees, tfs.euler_from_matrix(mat))
+    print '<origin xyz="{0[0]} {0[1]} {0[2]}" rpy="${{{1[0]}*pi/180}} ${{{1[1]}*pi/180}} ${{{1[2]}*pi/180}}"/>'.format(
+        xyz, rpy)
+    q = tfs.quaternion_from_matrix(mat)
+    print(xyz, q)
+
 
 def print_camera_pose(root_bot, bot_opt, opt_body):
-  transformer = TransformerROS()
+    transformer = TransformerROS()
 
-  # Print ground/effector <- camera(body) transformation
-  mat = tfs.concatenate_matrices(
-            transformer.fromTranslationRotation(*root_bot),
-            transformer.fromTranslationRotation(*bot_opt),
-            transformer.fromTranslationRotation(*opt_body))
-  print '\n=== Estimated ground/effector <- camera(body) transformation ==='
-  print_mat(mat)
+    # Print ground/effector <- camera(body) transformation
+    mat = tfs.concatenate_matrices(
+        transformer.fromTranslationRotation(*root_bot),
+        transformer.fromTranslationRotation(*bot_opt),
+        transformer.fromTranslationRotation(*opt_body))
+    print '\n=== Estimated ground/effector <- camera(body) transformation ==='
+    print_mat(mat)
+
 
 #########################################################################
 #  main part                                                            #
 #########################################################################
 if __name__ == '__main__':
-  rospy.init_node('o2as_handeye_calibration_publisher')
+    rospy.init_node('o2as_handeye_calibration_publisher')
 
-  while rospy.get_time() == 0.0:
-    pass
+    while rospy.get_time() == 0.0:
+        pass
 
-  # Get camera(optical) <- camera(body) transformation
-  opt_body = get_transform(rospy.get_param('camera_optical_frame'),
-                           rospy.get_param('camera_body_frame'))
+    # Get camera(optical) <- camera(body) transformation
+    opt_body = get_transform(rospy.get_param('camera_optical_frame'),
+                             rospy.get_param('camera_body_frame'))
 
-  # Estimated robot(effector/base) <- camera(optical) transformation
-  transformation, root_frame = get_estimated_transformation()
-  bot_opt   = ((transformation.transform.translation.x,
+    # Estimated robot(effector/base) <- camera(optical) transformation
+    transformation, root_frame = get_estimated_transformation()
+    bot_opt = ((transformation.transform.translation.x,
                 transformation.transform.translation.y,
                 transformation.transform.translation.z),
                (transformation.transform.rotation.x,
                 transformation.transform.rotation.y,
                 transformation.transform.rotation.z,
                 transformation.transform.rotation.w))
-  bot_frame = transformation.header.frame_id  # real effector/base frame
-  opt_frame = transformation.child_frame_id   # real camera(optical)_frame
+    bot_frame = transformation.header.frame_id  # real effector/base frame
+    opt_frame = transformation.child_frame_id  # real camera(optical)_frame
 
-  # Get robot(effector)/ground <- robot(effector/base) transformation
-  root_bot = get_transform(root_frame, bot_frame)
+    # Get robot(effector)/ground <- robot(effector/base) transformation
+    root_bot = get_transform(root_frame, bot_frame)
 
-  try:
-    broad = TransformBroadcaster()
-    rate  = rospy.Rate(50)
+    try:
+        broad = TransformBroadcaster()
+        rate = rospy.Rate(50)
 
-    while not rospy.is_shutdown():
-      now = rospy.Time.now()                           # child -> # parent
-      broad.sendTransform(bot_opt[0], bot_opt[1], now, opt_frame, bot_frame)
-      rate.sleep()
+        while not rospy.is_shutdown():
+            now = rospy.Time.now()  # child -> # parent
+            broad.sendTransform(bot_opt[0], bot_opt[1], now, opt_frame,
+                                bot_frame)
+            rate.sleep()
 
-    print_camera_pose(root_bot, bot_opt, opt_body)
+        print_camera_pose(root_bot, bot_opt, opt_body)
 
-  except rospy.ROSInterruptException:
-    print_camera_pose(root_bot, bot_opt, opt_body)
+    except rospy.ROSInterruptException:
+        print_camera_pose(root_bot, bot_opt, opt_body)
