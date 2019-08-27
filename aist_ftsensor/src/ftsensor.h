44dc0a956fd7715c2bbfc2bcfde1916f653025c1
Jun MIYOSHI <j.miyoshi@aist.go.jp>
2019-08-27 17:34:17 +0900

    Added bias of force-sensor. (torque)

--- aist_ftsensor/src/ftsensor.h
+++ aist_ftsensor/src/ftsensor.h
@@ -57,35 +57,22 @@ class ftsensor
     std::string			_sensor_frame;
     double			_rate;
     double			_m;
-    Eigen::Vector3f		_f0;
-    Eigen::Vector3f		_m0;
-    Eigen::Vector3f		_r0;
+    Eigen::Matrix<double, 3, 1>	_f0;
+    Eigen::Matrix<double, 3, 1>	_m0;
+    Eigen::Matrix<double, 3, 1>	_r0;
 
     const ros::ServiceServer	_take_sample;
     const ros::ServiceServer	_compute_calibration;
     const ros::ServiceServer	_save_calibration;
     bool			_get_sample;
-    Eigen::Matrix4f		_At_A;
-    Eigen::Vector4f		_At_b;
+    Eigen::Matrix<double, 4, 4>	_At_A;	    // force
+    Eigen::Matrix<double, 4, 1>	_At_b;	    // force
+    Eigen::Matrix<double, 6, 6>	_Ct_C;	    // torque
+    Eigen::Matrix<double, 6, 1>	_Ct_d;	    // torque
 
-    void	take_sample(const Eigen::Vector3f& k,
-			    const geometry_msgs::Vector3& f)		;
-
-#ifdef __MY_DEBUG__
-    void showMatrix3x3(const std::string& msg, const tf::Matrix3x3& m)
-    {
-	ROS_INFO("%s [%f %f %f][%f %f %f][%f %f %f]",
-		msg.c_str(),
-		m[0].x(), m[0].y(), m[0].z(),
-		m[1].x(), m[1].y(), m[1].z(),
-		m[2].x(), m[2].y(), m[2].z());
-    }
-    void showVector3(const std::string& msg, const geometry_msgs::Vector3& v)
-    {
-	ROS_INFO("%s [%f %f %f]", msg.c_str(), v.x, v.y, v.z);
-    }
-#endif /* __MY_DEBUG__ */
-
+    void	take_sample(const Eigen::Matrix<double, 3, 1>& k,
+			    const geometry_msgs::Vector3& f,
+			    const geometry_msgs::Vector3& m)		;
 };
 
 }	// namespace aist_ftsensor
