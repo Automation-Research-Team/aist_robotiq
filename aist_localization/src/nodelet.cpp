c536ff23e538b3f72ada9b18f87f9592877a19dc
Toshio UESHIBA <t.ueshiba@iast.go.jp>
2020-01-20 06:27:42 +0900

    WIP: initial commit of aist_localization.

---
+++ aist_localization/src/nodelet.cpp
@@ -0,0 +1,36 @@
+/*!
+ *  \file	nodelet.cpp
+ */
+#include "Localization.h"
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+
+namespace aist_localization
+{
+/************************************************************************
+*  class DetectorNodelet						*
+************************************************************************/
+class LocalizationNodelet : public nodelet::Nodelet
+{
+  public:
+			LocalizationNodelet()				{}
+
+    virtual void	onInit()					;
+
+  private:
+    ros::NodeHandle			_nh;
+    boost::shared_ptr<Localization>	_node;
+};
+
+void
+LocalizationNodelet::onInit()
+{
+    NODELET_INFO("aist_localization::LocalizationNodelet::onInit()");
+    _nh = getNodeHandle();
+
+    _node.reset(new Localization("aist_localization"));
+}
+
+}	// namespace aist_localization
+
+PLUGINLIB_EXPORT_CLASS(aist_localization::LocalizationNodelet, nodelet::Nodelet);
