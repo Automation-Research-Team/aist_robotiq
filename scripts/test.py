9fbb597252db4d6d21dcc5e8cc0ac1bb548690f7
Toshio UESHIBA <t.ueshiba@iast.go.jp>
2020-02-02 03:39:16 +0900

    Supported preemtion in localization.

--- scripts/test.py
+++ scripts/test.py
@@ -1,38 +1,117 @@
-import sys, os, collections
-import rospy, rospkg, rosparam
+#!/usr/bin/env python
+
+import os, sys, rospy, rospkg, argparse
 import actionlib
-import numpy as np
-import std_msgs
-import base
-from aist_localization import msg as amsg
+import xacro
+from std_srvs.srv      import Trigger
+from aist_localization import msg as lmsg
+from operator          import itemgetter
+
+#########################################################################
+#  class URDFSapwner                                                    #
+#########################################################################
+class URDFSpawner(object):
+    _template = ""
+    def __init__(self, urdf_dir):
+        super(URDFSpawner, self).__init__()
+
+        self._urdf_dir = urdf_dir
+
+    def add(self, pose, model, param=False):
+        macro_file = os.path.join(self._urdf_dir, model + "_macro.urdf.xacro")
+        doc = xacro.process_file(macro_file)
+        sys.stdout.write(doc.toprettyxml(indent='  ', encoding='utf8'))
 
 #########################################################################
 #  class LocalizationClient                                             #
 #########################################################################
 class LocalizationClient(object):
-
     def __init__(self):
         super(LocalizationClient, self).__init__()
 
-        self._localize = actionlib.SimpleActionClient(
-                           "/aist_localization/localize", amsg.LocalizeAction)
+        self._load_scene \
+            = rospy.ServiceProxy("localization/load_scene", Trigger)
+        self._localize = actionlib.SimpleActionClient("localization/localize",
+                                                      lmsg.localizeAction)
+        self._localize.wait_for_server()
 
-    def send_goal(self):
-        goal = amsg.LocalizeGoal()
-        goal.object_name = object_name
-        goal.timeout     = timeout
+    def load_scene(self):
+        return self._load_scene().success
+
+    def send_goal(self, object_name, number_of_poses=1):
+        self._poses    = []
+        self._overlaps = []
+        goal = lmsg.localizeGoal()
+        goal.object_name     = object_name
         goal.number_of_poses = number_of_poses
         self._localize.send_goal(goal, feedback_cb=self._feedback_cb)
 
-    def wait_for_result(self):
-        self._localize.wait_for_result()
-        result = self._localize.get_result()
-        return (self._compute_poses(result), result.overlabs)
+    def wait_for_result(self, timeout=5):
+        if (not self._localize.wait_for_result(timeout)):
+            self._localize.cancel_goal()  # Cancel goal if timeout expired.
 
-    def cancel_goal(self):
-        self._localize.cancel_goal()
+        # Sort obtained poses in descending order of overlap values.
+        pairs = sorted(zip(self._poses, self._overlaps),
+                       key=itemgetter(1), reverse=True)
+        if len(pairs):
+            self._poses, self._overlaps = zip(*pairs)
+
+        result = self._localize.get_result()
+        return (self._poses, self._overlaps,
+                result.success if result else False)
 
     def _feedback_cb(self, feedback):
-        pass
+        self._poses.append(feedback.pose)
+        self._overlaps.append(feedback.overlap)
 
-    def _compute_poses(self, res):
+
+######################################################################
+#  global functions                                                  #
+######################################################################
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test localizeAction')
+    parser.add_argument('-m',
+                        '--model',
+                        action='store',
+                        nargs='?',
+                        default='04_37D-GEARMOTOR-50-70',
+                        type=str,
+                        choices=None,
+                        help='name of model to be matched',
+                        metavar=None)
+    parser.add_argument('-n',
+                        '--number_of_poses',
+                        action='store',
+                        nargs='?',
+                        default=1,
+                        type=int,
+                        choices=None,
+                        help='the number of candidate poses',
+                        metavar=None)
+    parser.add_argument('-t',
+                        '--timeout',
+                        action='store',
+                        nargs='?',
+                        default=5,
+                        type=float,
+                        choices=None,
+                        help='timeout value',
+                        metavar=None)
+    args = parser.parse_args()
+
+    rospy.init_node('localization_client')
+
+    localize = LocalizationClient()
+    localize.load_scene()
+    localize.send_goal(args.model, args.number_of_poses)
+    (poses, overlaps, success) \
+        = localize.wait_for_result(rospy.Duration(args.timeout))
+
+    rospack = rospkg.RosPack()
+    urdf_dir = rospack.get_path("o2as_parts_description") + "/urdf/generated"
+
+    spawner = URDFSpawner(urdf_dir)
+
+    for pose, overlap in zip(poses, overlaps):
+        print("{}\noverlap: {}".format(pose, overlap))
+        spawner.add(pose, args.model)
--- src/Localization.cpp
+++ src/Localization.cpp
@@ -4,6 +4,7 @@
  *  \brief	ROS wrapper of Photoneo Localization SDK
  */
 #include <ros/package.h>
+#include <tf/tf.h>
 #include "Localization.h"
 
 namespace aist_localization
@@ -13,28 +14,26 @@ namespace aist_localization
 ************************************************************************/
 Localization::Localization(const std::string& name)
     :_nh(name),
-     _tfBroadcaster(),
-     _camera_frame("camera_frame"),
-     _config_dir(""),
+     _camera_frame("world"),
+     _plcf_dir(""),
      _localization(new pho::sdk::PhoLocalization()),
      _scene(),
      _scene_is_valid(false),
      _load_scene_srv(_nh.advertiseService("load_scene", &load_scene_cb, this)),
-     _localize_srv(_nh, "localize_from_ply",
+     _localize_srv(_nh, "localize",
 		   boost::bind(&Localization::localize_cb, this, _1), false)
 {
-    _nh.param("camera_frame", _camera_frame, std::string("camera_frame"));
-    _nh.param("config_dir",   _config_dir,
-	      ros::package::getPath("aist_localization") + "/config");
+    _localize_srv.registerPreemptCallback(
+	boost::bind(&Localization::preempt_cb, this));
     _localize_srv.start();
+
+    _nh.param("camera_frame", _camera_frame, std::string("world"));
+    _nh.param("plcf_dir", _plcf_dir,
+	      ros::package::getPath("aist_localization") + "/plcf");
 }
 
-Localization::~Localization()
-{
-}
-    
 void
-Localization::run()
+Localization::run() const
 {
     ros::spin();
 }
@@ -46,8 +45,8 @@ Localization::load_scene_cb(std_srvs::Trigger::Request&  req,
     try
     {
 	const auto	scene_file = scene_dir() + "/scene.ply";
-	
-	ROS_INFO_STREAM("load_scene: [" << scene_file << "]..,");
+
+	ROS_INFO_STREAM("Loading_scene[" << scene_file << "]..,");
 
 	_scene = pho::sdk::SceneSource::File(scene_file);
 	_localization->SetSceneSource(_scene);
@@ -64,102 +63,98 @@ Localization::load_scene_cb(std_srvs::Trigger::Request&  req,
 
     res.success = _scene_is_valid;
     res.message = (res.success ? "succeded" : "failed");
-    
+
     return true;
 }
-    
+
 void
-Localization::localize_cb(const goal_cp& goal)
+Localization::localize_cb(const goal_cp& goal) const
 {
     try
     {
-	ROS_INFO_STREAM("localize: Executing for object["
-			<< goal->object_name << "].");
+	ROS_INFO_STREAM("Localizing object[" << goal->object_name << "].");
 
 	if (!_scene_is_valid)
 	    throw std::runtime_error("Scene is not set.");
 
       // Load configuration.
-	const auto	config_file = _config_dir + '/'
-				    + goal->object_name + ".plcf";
-	if (!_localization->LoadLocalizationConfiguration(config_file))
+	const auto	plcf_file = _plcf_dir + '/'
+				  + goal->object_name + ".plcf";
+	if (!_localization->LoadLocalizationConfiguration(plcf_file))
 	    throw std::runtime_error("Failed to load configuration["
-				     + config_file + "].");
-	
+				     + plcf_file + "].");
+
       // Set stop criteria.
-	_localization->SetStopCriterion(
-	    pho::sdk::StopCriterion::Timeout(int(1000 * goal->timeout)));
 	_localization->SetStopCriterion(
 	    pho::sdk::StopCriterion::NumberOfResults(goal->number_of_poses));
 
       // Execute localization.
-	std::vector<pho::sdk::LocalizationPose>	locPoses;
-
+	const auto	now = ros::Time::now();
 	for (auto queue = _localization->StartAsync();
 	     queue.Size() != goal->number_of_poses; )
 	{
-	    if (_localize_srv.isPreemptRequested() || !ros::ok())
+	    if (_localize_srv.isPreemptRequested())
 	    {
-		ROS_INFO_STREAM("locallize: Preempted.");
-
-		_localize_srv.setPreempted();
-		break;
+		_localization->StopAsync();
+		return;
 	    }
 
 	    pho::sdk::LocalizationPose	locPose;
 	    if (queue.GetNext(locPose, 500))	// Timeout = 500ms
-		locPoses.push_back(locPose);
-	
-	  // Feedback the number of poses currently found.
-	    feedback_t	feedback;
-	    feedback.number_of_poses = locPoses.size();
-	    _localize_srv.publishFeedback(feedback);
+	    {
+		// const auto object_frame = ros::this_node::getNamespace()
+		// 			+ '/' + goal->object_name + '_'
+		// 			+ std::to_string(queue.Size() - 1);
+		const auto object_frame = goal->object_name + '_'
+					+ std::to_string(queue.Size() - 1);
+		publish_feedback(locPose, now, object_frame);
+	    }
 	}
-	
+
 	_localization->StopAsync();
 
-      // Sort obtained poses in descending order of overlap values.
-	std::sort(locPoses.begin(), locPoses.end(),
-		  [](const auto& a, const auto& b)
-		  { return a.VisibleOverlap > b.VisibleOverlap; });
-	
-      // Broadcast and save the obtained candidate poses.
 	result_t	result;
-	result.header.stamp	= ros::Time::now();
-	result.header.frame_id	= _camera_frame;
-	for (size_t i = 0; i < locPoses.size(); ++i)
-	{
-	  // Get transformation from the object to the camera.
-	    const auto&		mat = locPoses[i].Transformation;
-	    const tf::Transform	transform(tf::Matrix3x3(
-					      mat[0][0], mat[0][1], mat[0][2],
-					      mat[1][0], mat[1][1], mat[1][2],
-					      mat[2][0], mat[2][1], mat[2][2]),
-					  tf::Vector3(0.001*mat[0][3],
-						      0.001*mat[1][3],
-						      0.001*mat[2][3]));
-
-	  // Broadcast the cadidate pose as a frame.
-	    const auto	object_frame = ros::this_node::getNamespace() + '/'
-				     + goal->object_name + std::to_string(i);
-	    _tfBroadcaster.sendTransform({transform,
-					  result.header.stamp,
-					  result.header.frame_id,
-					  object_frame});
-
-	  // Save the candidate pose to the result.
-	    geometry_msgs::Pose	pose;
-	    tf::poseTFToMsg(transform, pose);
-	    result.poses.push_back(pose);
-	    result.overlaps.push_back(locPoses[i].VisibleOverlap);
-	}
-	
+	result.success = true;
 	_localize_srv.setSucceeded(result);
     }
     catch (const std::exception& err)
     {
-	ROS_ERROR_STREAM("localize_from_ply: " << err.what());
+	ROS_ERROR_STREAM(err.what());
+
+	_localization->StopAsync();
+
+	result_t	result;
+	result.success = false;
+	_localize_srv.setAborted(result);
     }
+}
+
+void
+Localization::preempt_cb() const
+{
+    ROS_INFO_STREAM("Localization preempted.");
+    _localize_srv.setPreempted();
+}
+
+void
+Localization::publish_feedback(const pho::sdk::LocalizationPose& locPose,
+			       ros::Time time,
+			       const std::string& object_frame) const
+{
+    const auto&		mat = locPose.Transformation;
+    const tf::Transform	transform(tf::Matrix3x3(
+				      mat[0][0], mat[0][1], mat[0][2],
+				      mat[1][0], mat[1][1], mat[1][2],
+				      mat[2][0], mat[2][1], mat[2][2]),
+				  tf::Vector3(0.001*mat[0][3],
+					      0.001*mat[1][3],
+					      0.001*mat[2][3]));
+    feedback_t		feedback;
+    feedback.pose.header.stamp	  = time;
+    feedback.pose.header.frame_id = _camera_frame;
+    tf::poseTFToMsg(transform, feedback.pose.pose);
+    feedback.overlap = locPose.VisibleOverlap;
+    _localize_srv.publishFeedback(feedback);
 }
 
 std::string
@@ -167,9 +162,9 @@ Localization::scene_dir()
 {
     const auto	home = getenv("HOME");
     if (!home)
-	throw std::runtime_error("Environment variable[HOME] is not set.");
+    	throw std::runtime_error("Environment variable[HOME] is not set.");
 
     return home + std::string("/.ros") + ros::this_node::getNamespace();
 }
-    
+
 }	// namespace aist_localization
