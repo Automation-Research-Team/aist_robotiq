2c47107d28dde8325d7c73f1dce135e268a68626
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2020-03-25 19:36:04 +0900

    Added test code for wrist cameras

--- aist_routines/src/aist_routines/CameraClient.py
+++ aist_routines/src/aist_routines/CameraClient.py
@@ -132,13 +132,16 @@ class RealSenseCamera(DepthCamera):
         self._dyn_camera = dynamic_reconfigure.client.Client(name, timeout=5.0)
         self._dyn_sensor = dynamic_reconfigure.client.Client(
                                name + "/coded_light_depth_sensor", timeout=5.0)
-        self._recent_laser_power = 16
+        current_laser_power = self.laser_power
+        if current_laser_power != 0:
+            self._recent_laser_power = current_laser_power
+        else:
+            self._recent_laser_power = 16
 
     @property
     def laser_power(self):
         print("{}: get laser power".format(self.name))
         ret = self._dyn_sensor.get_configuration()
-        pirnt(ret)
         return ret["laser_power"]
 
     @laser_power.setter
@@ -147,15 +150,15 @@ class RealSenseCamera(DepthCamera):
         self._dyn_sensor.update_configuration({"laser_power" : value})
 
     def continuous_shot(self, enabled):
-        # if enabled:
-        #     self.laser_power = self._recent_laser_power
-        # else:
-        #     current_laser_power = self.laser_power
-        #     if current_laser_power != 0:
-        #         self._recent_laser_power = current_laser_power
-        #     self.laser_power = 0
-        print("{}: set enable_streaming to {}".format(self.name, enabled))
+        if enabled:
+            self.laser_power = self._recent_laser_power
+        else:
+            current_laser_power = self.laser_power
+            if current_laser_power != 0:
+                self._recent_laser_power = current_laser_power
+            self.laser_power = 0
         self._dyn_camera.update_configuration({"enable_streaming" : enabled})
+        rospy.sleep(0.1)
         return True
 
 ######################################################################
