5ae00aa370f0ca3e9616e2306b232cab8d9c91e1
avasalya <ashesh.vasalya@aist.go.jp>
2020-12-02 16:23:52 +0900

    added support for realsenseD435 to work with UR5 --handeye calibration tested using aruco marker ID=32, size=0.16, margin=0.01 --onigiri picking experiment performed using yol6d pose estimation --for this  script adapted -- aist_routines->base.py -->def (effector_target_pose) change manually for now between gmsg.PoseStamped or gmsg.PoseArray

--- aist_handeye_calibration/scripts/check_calibration.py
+++ aist_handeye_calibration/scripts/check_calibration.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 import rospy
 from math               import radians
@@ -15,16 +15,16 @@ class CheckCalibrationRoutines(AISTBaseRoutines):
     def __init__(self):
         super(CheckCalibrationRoutines, self).__init__()
         self._camera_name      = rospy.get_param('~camera_name',
-                                                 'a_phoxi_m_camera')
+                                                'realsenseD435')
         self._robot_name       = rospy.get_param('~robot_name', 'b_bot')
         self._robot_base_frame = rospy.get_param('~robot_base_frame',
-                                                 'workspace_center')
+                                                'workspace_center')
         self._robot_effector_frame \
-                               = rospy.get_param('~robot_effector_frame',
-                                                 'b_bot_ee_link')
+                                = rospy.get_param('~robot_effector_frame',
+                                                'b_bot_ee_link')
         self._robot_effector_tip_frame \
-                               = rospy.get_param('~robot_effector_tip_frame',
-                                                 '')
+                                = rospy.get_param('~robot_effector_tip_frame',
+                                                '')
         self._initpose         = rospy.get_param('~initpose', [])
         self._speed            = rospy.get_param('~speed', 1)
 
@@ -32,7 +32,7 @@ class CheckCalibrationRoutines(AISTBaseRoutines):
         poseStamped = gmsg.PoseStamped()
         poseStamped.header.frame_id = self._robot_base_frame
         poseStamped.pose = gmsg.Pose(gmsg.Point(*pose[0:3]),
-                                     gmsg.Quaternion(
+                                    gmsg.Quaternion(
                                          *tfs.quaternion_from_euler(
                                              *map(radians, pose[3:6]))))
         print('  move to ' + self.format_pose(poseStamped))
@@ -47,14 +47,14 @@ class CheckCalibrationRoutines(AISTBaseRoutines):
     def move_to_marker(self):
         self.trigger_frame(self._camera_name)
         marker_pose = rospy.wait_for_message('/aruco_detector/pose',
-                                             gmsg.PoseStamped, 10)
+                                            gmsg.PoseStamped, 10)
         approach_pose = self.effector_target_pose(marker_pose, (0, 0, 0.05))
 
         #  We have to transform the target pose to reference frame before moving
         #  to the approach pose because the marker pose is given w.r.t. camera
         #  frame which will change while moving in the case of "eye on hand".
         target_pose = self.transform_pose_to_reference_frame(
-                          self.effector_target_pose(marker_pose, (0, 0, 0)))
+                        self.effector_target_pose(marker_pose, (0, 0, 0)))
         print('  move to ' + self.format_pose(approach_pose))
         (success, _, current_pose) \
             = self.go_to_pose_goal(
