3d4d559c71c769c229b87ee0a210d2e37ba11ba5
Toshio UESHBIA <t.ueshiba@aist.go.jp>
2021-04-10 16:32:07 +0900

    Recover aist_robotiq package

--- aist_routines/src/aist_routines/iiwa.py
+++
@@ -1,9 +0,0 @@
-import rospy
-from aist_routines import AISTBaseRoutines
-
-######################################################################
-#  class IiwaRoutines                                                #
-######################################################################
-class IiwaRoutines(AISTBaseRoutines):
-    def __init__(self):
-        super(IiwaRoutines, self).__init__()
